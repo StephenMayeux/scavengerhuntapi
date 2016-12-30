3622b60c62ebb72d50aae9fe03ac9f46f80b1de9
Stephen Mayeux <stephenmayeux@gmail.com>
2016-12-30 13:03:28 -0600

    create game model

---
+++ models/game.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define the model
+const gameSchema = new Schema({
+  students: { type: Array, default: [] },
+  hunt_id: { type: Schema.Types.ObjectId, ref: 'hunt', required: true },
+  completed: { type: Boolean, default: false },
+  responses: { type: Array, default: [] }
+}, { timestamps: true });
+
+const modelClass = mongoose.model('game', gameSchema);
+module.exports = modelClass;
